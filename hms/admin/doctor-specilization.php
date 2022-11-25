495ee0b863564df8420b67ed64b5f818b39d933e
garber batista <garbfbatista@gmail.com>
2022-11-25 16:21:25 -0400

    add

--- hms/admin/doctor-specilization.php
+++ hms/admin/doctor-specilization.php
@@ -6,20 +6,20 @@ include('include/checklogin.php');
 check_login();
 if(isset($_POST['submit']))
 {
-$sql=mysqli_query($con,"insert into doctorSpecilization(specilization) values('".$_POST['doctorspecilization']."')");
-$_SESSION['msg']="Doctor Specialization added successfully !!";
+$sql=mysqli_query($con,"insert into especializacion(doctor_especializacion) values('".$_POST['doctor_especializacion']."')");
+$_SESSION['msg']="Doctor Especialización agregada con éxito !!";
 }
 
 if(isset($_GET['del']))
 		  {
-		          mysqli_query($con,"delete from doctorSpecilization where id = '".$_GET['id']."'");
-                  $_SESSION['msg']="data deleted !!";
+		          mysqli_query($con,"Eliminar de especializacion donde id = '".$_GET['id']."'");
+                  $_SESSION['msg']="Datos borrados!!";
 		  }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 	<head>
-		<title>Admin | Doctor Specialization</title>
+		<title>Admin | Doctora Especialización</title>
 	
 		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
 		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
@@ -49,17 +49,9 @@ if(isset($_GET['del']))
 						<!-- start: PAGE TITLE -->
 						<section id="page-title">
 							<div class="row">
-								<div class="col-sm-8">
-									<h1 class="mainTitle">Admin | Add Doctor Specialization</h1>
-																	</div>
-								<ol class="breadcrumb">
-									<li>
-										<span>Admin</span>
-									</li>
-									<li class="active">
-										<span>Add Doctor Specialization</span>
-									</li>
-								</ol>
+								<div class="col-sm-12">
+									<h1 class="mainTitle">Admin | Agregar doctora Especialización</h1>
+									</div>
 							</div>
 						</section>
 						<!-- end: PAGE TITLE -->
@@ -72,24 +64,24 @@ if(isset($_GET['del']))
 										<div class="col-lg-6 col-md-12">
 											<div class="panel panel-white">
 												<div class="panel-heading">
-													<h5 class="panel-title">Doctor Specialization</h5>
+													<h5 class="panel-title">Doctor especializacion</h5>
 												</div>
 												<div class="panel-body">
-								<p style="color:red;"><?php echo htmlentities($_SESSION['msg']);?>
-								<?php echo htmlentities($_SESSION['msg']="");?></p>	
+								               <p style="color:green;"><?php echo htmlentities($_SESSION['msg']);?>
+								                <?php echo htmlentities($_SESSION['msg']="");?></p>	
 													<form role="form" name="dcotorspcl" method="post" >
 														<div class="form-group">
 															<label for="exampleInputEmail1">
-																Doctor Specialization
+																Doctor especializacion
 															</label>
-							<input type="text" name="doctorspecilization" class="form-control"  placeholder="Enter Doctor Specialization">
+							                             <input type="text" name="doctor_especializacion" class="form-control"  placeholder="Ingresar Doctor doctor especializacion">
 														</div>
 												
 														
 														
 														
 														<button type="submit" name="submit" class="btn btn-o btn-primary">
-															Submit
+														Enviar
 														</button>
 													</form>
 												</div>
@@ -108,33 +100,31 @@ if(isset($_GET['del']))
 
 									<div class="row">
 								<div class="col-md-12">
-									<h5 class="over-title margin-bottom-15">Manage <span class="text-bold">Docter Specialization</span></h5>
+									<h5 class="over-title margin-bottom-15"> <span class="text-bold">Doctor/a  Especialización</span></h5>
 									
 									<table class="table table-hover" id="sample-table-1">
 										<thead>
 											<tr>
 												<th class="center">#</th>
-												<th>Specialization</th>
-												<th class="hidden-xs">Creation Date</th>
-												<th>Updation Date</th>
-												<th>Action</th>
+												<th>Especialización</th>
+												<th class="hidden-xs">Fecha de creación</th>
+												<th>Accion</th>
 												
 											</tr>
 										</thead>
 										<tbody>
-<?php
-$sql=mysqli_query($con,"select * from doctorSpecilization");
-$cnt=1;
-while($row=mysqli_fetch_array($sql))
-{
-?>
+                                        <?php
+                                        $sql=mysqli_query($con,"select * from especializacion");
+                                        $cnt=1;
+                                        while($row=mysqli_fetch_array($sql))
+                                        {
+                                        ?>
 
 											<tr>
 												<td class="center"><?php echo $cnt;?>.</td>
-												<td class="hidden-xs"><?php echo $row['specilization'];?></td>
-												<td><?php echo $row['creationDate'];?></td>
-												<td><?php echo $row['updationDate'];?>
-												</td>
+												<td class="hidden-xs"><?php echo $row['doctor_especializacion'];?></td>
+												<td><?php echo $row['fecha_creación'];?></td>
+											
 												
 												<td >
 												<div class="visible-md visible-lg hidden-sm hidden-xs">
@@ -150,17 +140,17 @@ while($row=mysqli_fetch_array($sql))
 														<ul class="dropdown-menu pull-right dropdown-light" role="menu">
 															<li>
 																<a href="#">
-																	Edit
+																	Editar
 																</a>
 															</li>
 															<li>
 																<a href="#">
-																	Share
+																Cuota
 																</a>
 															</li>
 															<li>
 																<a href="#">
-																	Remove
+																Remover
 																</a>
 															</li>
 														</ul>
